547b44b3dc7549a5841eb59f656ab685661fc1ab
Jorjman <dontes2012@gmail.com>
2019-01-26 12:30:40 +0200

    comments removed

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 
 ini_set('display_errors', 1);
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';// composer
+// composer
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-//include_once __DIR__ . '/migrations/dbCreateHistory.php';// попытка создать таблицу истории
+// подключает все основные классы core и модели
+require_once dirname(__FILE__) . '/app/bootstrap.php';
 
-//include_once __DIR__ . '/migrations/dbCreateSettings.php';// попытка создать таблицу настроек
-
-require_once dirname(__FILE__) . '/app/bootstrap.php';// подключает все основные классы core и модели
-
-//require_once __DIR__ . '/migrations/dbDropHistory.php';
-
-//require_once __DIR__ . '/migrations/dbDropSettings.php';
 
